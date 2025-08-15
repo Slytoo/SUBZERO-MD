63cc5d888273f433087d0d852f98bdf3c3df0029
Slytoo <sillietoo001@gmail.com>
2025-08-15 11:24:31 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,7 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;XIVBnShS#sUkCThqFOsbC7miAShSpFgSkVIqlKl1rM_k90W60qIE", //
+  SESSION_ID: "SUBZERO-MD;;;XIVBnShS#sUkCThqFOsbC7miAShSpFgSkVIqlKl1rM_k90W60qIE"
   OWNER_NUMBER: "254116519488"
-  PREFIX: ".", //
+  PREFIX: "."
   CDN: "https://mrfrankk-cdn.hf.space"
 };
 
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
